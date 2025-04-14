24b9074934d538f5c357ec9ee8c351e8a40e24d8
ethanrgibson <ethanrgibson33@gmail.com>
2025-04-14 14:10:30 -0600

    computer can choose random hand

--- Program.cs
+++ Program.cs
@@ -6,13 +6,14 @@
     Console.WriteLine("Rock, Paper, Scissors. Choose or Die!");
 
     string userHand = ChooseHand();
+    string computerHand = GetComputerHand();
 
   }
 
   static string ChooseHand()
   {
 
-Console.Clear();
+    Console.Clear();
 
     Console.WriteLine("Choose A Hand");
     Console.WriteLine("1. Rock");
@@ -21,11 +22,36 @@ Console.Clear();
 
     string? userInput = Console.ReadLine();
 
+
     if (userInput == "1")
     { Console.WriteLine("Player Chooses Rock"); }
+    if (userInput == "2")
+    { Console.WriteLine("Player Chooses Paper"); }
+    if (userInput == "3")
+    { Console.WriteLine("Player Chooses Scissors"); }
 
-return userInput;
+    if (userInput != "1" && userInput != "2" && userInput != "3")
+    {
+      Console.WriteLine("Please Select a number");
+      return ChooseHand();
+    }
 
+    return "";
   }
 
+  static string GetComputerHand()
+  {
+    int randomNumber = new Random().Next(1, 4);
+
+    if (randomNumber == 1)
+    { Console.WriteLine("Computer Chooses Rock"); }
+    if (randomNumber == 2)
+    { Console.WriteLine("Computer Chooses Paper"); }
+    if (randomNumber == 3)
+    { Console.WriteLine("Computer Chooses Scissors"); }
+
+
+    return "";
+
+  }
 }
